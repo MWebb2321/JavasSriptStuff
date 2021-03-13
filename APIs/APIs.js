4b2ceb18a0cb5930074050968783782d2a258143
Matthew Webb <mwebb2321@gmail.com>
2021-03-12 23:48:55 -0800

    added API folder and file

---
+++ APIs/APIs.js
@@ -0,0 +1,23 @@
+// User interfaces allow humans to interact with programs.
+// Application Programming Interfaces (APIs) let programs (including scripts) talk to each other.
+
+// Types of APIs
+
+// REST APIs
+// REST APIs, also known as RESTful APIs, stand for Representational State Transfer.
+// REST APIs have grown in popularity of late, as a part of Web Services.
+// REST APIs are designed for developers to perform requests and receive responses via HTTP functions.
+// There are four different HTTP commands that REST are based on. These include GET, PUT, POST, and DELETE.
+// When you pull up Instagram and search for the latest and greatest meme of the day, that app is using a REST API.
+
+// SOAP APIs
+// SOAP stands for Simple Object Access Protocol.
+// While REST is an architectural style, SOAP is a protocol which is defined by a standard.
+// SOAP is dependent on XML-based systems and programming, tending to have larger, more expensive data.
+// SOAP APIs also provide a higher level of security.
+// A common use case is an application interacting with a financial institution.
+
+// RCP APIs
+// RPC stands for Remote Procedure Call.
+// RPC APIs were the earliest form of APIs, as they are designed to execute a block of code on a different server.
+// When it is used over HTTP, it can become a Web API.
